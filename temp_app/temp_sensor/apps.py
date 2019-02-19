956ce1dfcdfa8148126bb98a025532cb46104ab2
jfetzer <jasonmfetzer@gmail.com>
2019-02-19 09:49:32 -0600

    Test Branch

---
+++ temp_app/temp_sensor/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TempSesensorConfig(AppConfig):
+    name = 'temp_sesensor'
---
+++ temp_app/temp_sensor/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class tempSensor(models.Model):
+	#
+	pass
+	
